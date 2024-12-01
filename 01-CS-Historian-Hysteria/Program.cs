ea6373697d3222cbf3c2df511a2e9561dbf69b5c
luksladky <lusladky@gmail.com>
2024-12-01 20:07:24 +0100

    01

---
+++ 01-CS-Historian-Hysteria/Program.cs
@@ -0,0 +1,47 @@
+﻿var projectRoot = Directory.GetParent(Directory.GetCurrentDirectory())?.Parent?.Parent?.FullName;
+var inputPath = Path.Combine(projectRoot!, "input.txt");
+
+// a min-heap, if value == priority
+var heapA = new PriorityQueue<int, int>();
+var heapB = new PriorityQueue<int, int>();
+
+// item -> count
+var countB = new Dictionary<int, int>();
+
+foreach (var line in File.ReadLines(inputPath))
+{
+    var lists = line.Split("   ", StringSplitOptions.TrimEntries);
+
+    // items from list A and B
+    var (itemA, itemB) = (int.Parse(lists[0]), int.Parse(lists[1]));
+    heapA.Enqueue(itemA, itemA);
+    heapB.Enqueue(itemB, itemB);
+
+    // ensure all keys from list A are present
+    if (!countB.ContainsKey(itemA))
+    {
+        countB[itemA] = 0;
+    }
+
+    if (!countB.ContainsKey(itemB))
+    {
+        countB[itemB] = 0;
+    }
+
+    countB[itemB]++;
+}
+
+int totalDist = 0;
+int totalSimilarity = 0;
+
+while (heapA.Count > 0 && heapB.Count > 0)
+{
+    var itemA = heapA.Dequeue();
+    var itemB = heapB.Dequeue();
+    
+    totalDist += Math.Abs(itemA - itemB);
+    totalSimilarity += countB[itemA] * itemA;
+}
+
+Console.WriteLine($"Total distance: {totalDist}");
+Console.WriteLine($"Total similarity: {totalSimilarity}");
---
+++ CsNotes/FileReading.cs
@@ -0,0 +1,53 @@
+﻿namespace CsNotes
+{
+    public class FileReading
+    {
+        /// <summary>
+        /// File.ReadLines, lazy
+        /// </summary>
+        /// <param name="fullPath"></param>
+        void ReadLines_1(string fullPath)
+        {
+            foreach (var line in File.ReadAllLines(fullPath))
+            {
+                ProcessLine(line);
+            }
+        }
+
+        /// <summary> 
+        /// StreamReader, lazy, flexible (encoding, etc)
+        /// </summary>
+        /// <param name="fullPath"></param>
+        void ReadLines_2(string fullPath)
+        {
+            using var sr = File.OpenText(fullPath);
+
+            string? line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                ProcessLine(line);
+            }
+        }
+
+
+        /// <summary> 
+        /// StreamReader, lazy, flexible (encoding, etc) + pattern matching
+        /// </summary>
+        /// <param name="fullPath"></param>
+        void ReadLines_3(string fullPath)
+        {
+            using var sr = File.OpenText(fullPath);
+
+            // { } is match any object -> a non-null value
+            while (sr.ReadLine() is { } line)
+            {
+                ProcessLine(line);
+            }
+        }
+
+        private void ProcessLine(string? line)
+        {
+            Console.WriteLine(line);
+        }
+    }
+}

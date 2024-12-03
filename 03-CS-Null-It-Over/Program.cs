7008461d7f0bb902a900733a3df4d11e362e239d
luksladky <lusladky@gmail.com>
2024-12-03 21:39:51 +0100

    03

---
+++ 03-CS-Null-It-Over/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+
+var projectRoot = Directory.GetParent(Directory.GetCurrentDirectory())?.Parent?.Parent?.FullName;
+var inputPath = Path.Combine(projectRoot!, "input.txt");
+
+var input  = File.ReadAllText(inputPath);
+
+input = Regex.Replace(input, "don't\\(\\).*?do\\(\\)", "", RegexOptions.Singleline);
+
+var matches = Regex.Matches(input, "mul\\((\\d+),(\\d+)\\)");
+int result = 0;
+foreach (Match match in matches)
+{
+    result += int.Parse(match.Groups[2].Value) * int.Parse(match.Groups[1].Value);
+}
+
+Console.WriteLine(result);
